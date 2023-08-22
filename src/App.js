6bc14256ffba9dff1a35a42f74e9ead9445ad0f3
Felix-Johannesson <felixjohannesson1@gmail.com>
2023-08-22 19:30:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import Home from "./pages/Home";
 import Kassa from "./pages/Kassa";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { RootStyles, BodyStyles } from "./components/StyleComponents";
+import { BodyStyles } from "./components/StyleComponents";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import BackToTopButton from "./components/BackToTopButton";
 
 function App() {
   return (
